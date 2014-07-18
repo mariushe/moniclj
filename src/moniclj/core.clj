83e1f2f768ea50fd0c06e872ea9ad25454a74226
Marius Herring <marius@medmgmt-4.tajen.edu.tw>
2014-07-18 23:10:17 +0200

    Started on the core

--- src/moniclj/core.clj
+++ src/moniclj/core.clj
@@ -1,3 +1,24 @@
-(ns moniclj.core)
+(ns moniclj.core
+  (use [clojure.java.shell :only [sh]]
+       [clojure.string :only [split]]))
 
-(def hello-world (fn [] "Hello world"))
+(def execute-check (fn [cmd] (sh "sh" "-c" cmd)))
+
+
+(def run-check (fn [check] (let [result (execute-check (-> check :cmd))]
+                             (assoc check :last-update {:state (-> result :exit)
+                                                        :msg (-> result :out)}))))
+
+(def failed? (fn [check] (= (-> check :last-update :state) 2)))
+
+(def init-or-inc (fn [value] (if-not (nil? value)
+                               (inc value)
+                               1)))
+
+(def inc-if-fail (fn [check]  (if (failed? check) 
+                                (update-in check [:fail-count] init-or-inc)
+                                (assoc check :fail-count 0))))
+
+(def handle-check (fn [check] (-> check 
+                                  (run-check)
+                                  (inc-if-fail))))
--- test/moniclj/test/core_test.clj
+++ test/moniclj/test/core_test.clj
@@ -4,5 +4,20 @@
             [midje.sweet :refer :all]
             [conjure.core :refer :all]))
 
-(fact "testing that setup works"
-      (core/hello-world) => "Hello world")
+(fact "execute shell command"
+      (core/execute-check "echo hello world") => {:exit 0 :out "hello world\n" :err ""})
+
+(fact "handle check"
+      (core/handle-check {:cmd "echo hello world" :last-update {}}) => {:cmd "echo hello world"
+                                                                        :fail-count 0
+                                                                        :last-update {:state 0
+                                                                                      :msg "hello world\n"}})
+
+(fact "nil when init-or-inc"
+      (core/init-or-inc nil) => 1)
+
+(fact "0 when init-or-inc"
+      (core/init-or-inc 0) => 1)
+
+(fact "1 when init-or-inc"
+      (core/init-or-inc 1) => 2)
