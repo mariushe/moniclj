3254baaaff4c29655266d33fdc26fd1c298ab9b9
Marius Herring <marius@medmgmt-4.tajen.edu.tw>
2014-07-22 19:27:56 +0200

    Started on frontend structure

--- src/moniclj/handler.clj
+++ src/moniclj/handler.clj
@@ -3,6 +3,7 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [moniclj.check-dao :as dao]
+            [ring.util.response :as resp]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.json :refer [wrap-json-body]]
             [ring.middleware.json :refer [wrap-json-response]]
@@ -11,8 +12,7 @@
 (def fix-object (fn [check] (assoc check :_id (str (check :_id)))))
 
 (defroutes app-routes
-  (GET "/" [] (resource :available-media-types ["text/html"]
-                        :handle-ok "<html>Hello, Internet.</html>"))
+  (GET "/" [] (resp/resource-response "index.html" {:root "app"}))
   (context "/api/check" []
            (GET "/" [] (resource :available-media-types ["application/json"]
                                  :handle-ok (map fix-object (dao/get-checks)))))
